90bfe57b264aacf78b1bc250643a82b94aca7e2e
Melvin PERRIN <melperri@e1r12p22.42.fr>
2021-01-25 14:18:59 +0100

    115% OK

---
+++ get_next_line_bonus.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/07 14:43:44 by melperri          #+#    #+#             */
+/*   Updated: 2021/01/07 15:38:09 by melperri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
+
+typedef struct	s_gnl
+{
+	char	buf[BUFFER_SIZE + 1];
+	int		ret;
+	char	*tmp;
+	int		i;
+	char	*tofree;
+}				t_gnl;
+
+char			*cpy_line(char *tmp);
+int				is_line(char *tmp);
+int				read_file(t_gnl *gnl, int fd);
+int				get_next_line(int fd, char **line);
+size_t			ft_strlen(const char *str);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strdup(const char *src);
+char			*ft_strchr(const char *s, int c);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
